6a37d89d515e8324504a815f9bc7cc15eff20453
ZASALAS <zasalastobon@STUDIOYAGENCIA.localdomain>
2023-01-11 18:52:28 -0500

    update color backgroud

--- pages/grafica/CargarDatos.js
+++ pages/grafica/CargarDatos.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import DataDisplayer from './DataDisplayer';
+
 import axios from 'axios';
-axios.defaults.withCredentials = true;  // Habilitamos las cookies de sesión en las solicitudes
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
 function CargarDatos() {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
