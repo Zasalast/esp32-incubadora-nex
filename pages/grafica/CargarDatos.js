d28dfba9fde402fbdc1c6dcf981766079760b958
ZASALAS <zasalastobon@STUDIOYAGENCIA.localdomain>
2023-01-11 13:20:05 -0500

    update dependecies package.json

---
+++ pages/grafica/CargarDatos.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import DataDisplayer from './DataDisplayer';
+import axios from 'axios';
+axios.defaults.withCredentials = true;  // Habilitamos las cookies de sesión en las solicitudes
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+function CargarDatos() {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('https://b2luadwf3k.execute-api.us-east-1.amazonaws.com/reads');
+                setData(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+        fetchData();
+       /*  async function fetchData() {
+            console.log("hola")
+            try {
+                const response = await fetch('https://b2luadwf3k.execute-api.us-east-1.amazonaws.com/reads').promise();
+                console.log("response", response)
+                const data = await response.json();
+                console.log("hola", data)
+                setData(data);
+            } catch (error) {
+                setError(error);
+            }
+        } */ fetchData();
+
+
+    }, []);
+    return (
+        <div>CargarDatos</div>
+    )
+}
+
+export default CargarDatos
---
+++ pages/grafica/Grafica.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import LineChart from "./LineChart.js";
+
+
+
+import { ClimaData, UserData, TemperaturaData, HumedadData } from "./Data.js";
+
+function Grafica() {
+  const datapoints = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+  const [climaData, setHumedadData] = useState({
+    labels: datapoints,
+    datasets: [
+      {
+        label: "Humedad",
+        data: ClimaData.map((data) => data.Humedad),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "black",
+        borderWidth: 2,
+      }, {
+        label: "Temperatura",
+        data: ClimaData.map((data) => data.Temperatura),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "#ecf0f1",
+          "#50AF95",
+          "#f3ba2f",
+          "#2a71d0",
+        ],
+        borderColor: "red",
+        borderWidth: 2,
+      },
+    ],
+  });
+
+
+  // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
+  return (
+    <div >
+      <div style={{ width: 700 }}>
+        <LineChart chartData={climaData} />
+      </div>
+    </div>
+  );
+}
+
+export default Grafica;
