ea0ed81f944a2de1693fc6dcba0ff0b455a5c466
ZASALAS <zasalastobon@STUDIOYAGENCIA.localdomain>
2023-01-11 23:01:28 -0500

    update data

--- pages/grafica/Grafica.js
+++ pages/grafica/Grafica.js
@@ -1,40 +1,43 @@
 import { useState } from "react";
 import LineChart from "./LineChart.js";
 
-import { ClimaData, UserData, TemperaturaData, HumedadData } from "../../Lib/Data.js";
+//import { ClimaData, UserData, TemperaturaData, HumedadData } from "../../Lib/Data.js";
 
 function Grafica() {
-  const datapoints = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
-  const [climaData, setHumedadData] = useState({
-    labels: datapoints,
-    datasets: [
-      {
-        label: "Humedad",
-        data: ClimaData.map((data) => data.Humedad),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      }, {
-        label: "Temperatura",
-        data: ClimaData.map((data) => data.Temperatura),
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "red",
-        borderWidth: 2,
-      },
-    ],
-  });
+  if (!data) {
+    const datapoints = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120];
+    const [climaData, setHumedadData] = useState({
+      labels: datapoints,
+      datasets: [
+        {
+          label: "Humedad",
+          data: ClimaData.map((data) => data.Humedad),
+          backgroundColor: [
+            "rgba(75,192,192,1)",
+            "#ecf0f1",
+            "#50AF95",
+            "#f3ba2f",
+            "#2a71d0",
+          ],
+          borderColor: "black",
+          borderWidth: 2,
+        }, {
+          label: "Temperatura",
+          data: ClimaData.map((data) => data.Temperatura),
+          backgroundColor: [
+            "rgba(75,192,192,1)",
+            "#ecf0f1",
+            "#50AF95",
+            "#f3ba2f",
+            "#2a71d0",
+          ],
+          borderColor: "red",
+          borderWidth: 2,
+        },
+      ],
+    });
+  }
+
 
 
 
